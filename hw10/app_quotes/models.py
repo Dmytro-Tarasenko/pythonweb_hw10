5abebcfff58521db303934561ac7b312a726581b
Dmytro-Tarasenko <ug.dotenko@gmail.com>
2024-03-29 16:23:45 +0200

    Update

--- hw10/app_quotes/models.py
+++ hw10/app_quotes/models.py
@@ -1,3 +1,36 @@
-from django.db import models
-
+from django.db.models import (Model,
+                              CharField,
+                              DateField,
+                              TextField,
+                              ManyToManyField,
+                              ForeignKey,
+                              ManyToOneRel,
+                              CASCADE)
 # Create your models here.
+
+
+class Tag(Model):
+    name = CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Quote(Model):
+    text = TextField()
+    author = ForeignKey('Author', on_delete=CASCADE)
+    tags = ManyToManyField(Tag)
+
+    def __str__(self):
+        return f"{self.author}: {self.text[:50]}"
+
+
+class Author(Model):
+    fulname = CharField(max_length=100)
+    birth_date = DateField()
+    birth_location = CharField(max_length=100)
+    description = TextField()
+    # quotes = ManyToOneRel(to=Quote, field='author')
+
+    def __str__(self):
+        return f"{self.name} {self.birth_date}"
