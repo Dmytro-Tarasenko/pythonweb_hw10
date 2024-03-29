30b7247046eda0d98972438fd7a59b971238289b
Dmytro-Tarasenko <ug.dotenko@gmail.com>
2024-03-29 14:37:12 +0200

    Update the repository with the latest changes

--- hw10/app_quotes/urls.py
+++ hw10/app_quotes/urls.py
@@ -5,5 +5,15 @@ from . import views
 app_name = "app_quotes"
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('authors/', views.authors, name='authors'),
+    path('authors/page/<int:page>', views.authors, name='authors'),
+    path('authors/<int:author_id>', views.authors, name='authors'),
+    path('authos/<str:name>', views.authors, name='authors'),
+    path('tags/', views.tags, name='tags'),
+    path('tags/<str:tag>', views.tags, name='tags'),
+    path('tags/page/<int:page>', views.tags, name='tags'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.register, name='register')
 ]
--- hw10/app_quotes/views.py
+++ hw10/app_quotes/views.py
@@ -6,3 +6,38 @@ def index(request):
     return render(request=request,
                   template_name='app_quotes/index.html',
                   context={'msg': "Quotes app index"})
+
+
+def authors(request,
+            page: int = 1,
+            author_id: int = None,
+            name: str = None):
+    return render(request=request,
+                  template_name='app_quotes/index.html',
+                  context={'msg': f"Author, page: {page}, id: {author_id}, name: {name}"})
+
+
+def tags(request,
+         tag: str = None,
+         page: int = 1):
+    return render(request=request,
+                  template_name='app_quotes/index.html',
+                  context={'msg': f"Tags, page: {page}, tag: {tag}"})
+
+
+def login(request):
+    return render(request=request,
+                  template_name='app_quotes/index.html',
+                  context={'msg': "Login page"})
+
+
+def logout(request):
+    return render(request=request,
+                  template_name='app_quotes/index.html',
+                  context={'msg': "Logout handler"})
+
+
+def register(request):
+    return render(request=request,
+                  template_name='app_quotes/index.html',
+                  context={'msg': "Signup page"})
