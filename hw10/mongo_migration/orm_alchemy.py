3329245f2a154c6d83dfaaf8dd00799df456ab9e
dmytrot <ug.dotenko@gmail.com>
2024-03-31 20:38:32 +0300

    update

--- hw10/mongo_migration/orm_alchemy.py
+++ hw10/mongo_migration/orm_alchemy.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-engine = create_engine()
+engine = create_engine('postgresql://guest:guest@localhost:5432/hw10')
 
 Base = declarative_base()
 
@@ -30,7 +30,7 @@ class QuoteSQL(Base):
     __tablename__ = "quotes"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    author: Mapped[int] = mapped_column(ForeignKey("authors.id"))
+    author: Mapped[int] = mapped_column(ForeignKey("authors.id"), nullable=True)
     quote: Mapped[str] = mapped_column(nullable=False)
     tags: Mapped[list["TagSQL"]] = relationship(
         back_populates="quotes"
@@ -44,6 +44,23 @@ class AuthorSQL(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     fullname: Mapped[str] = mapped_column(nullable=False,
                                          unique=True)
-    birth_date: Mapped[str] = mapped_column()
-    birth_location: Mapped[str] = mapped_column()
-    description: Mapped[str] = mapped_column()
+    birth_date: Mapped[str] = mapped_column(nullable=True)
+    birth_location: Mapped[str] = mapped_column(nullable=True)
+    description: Mapped[str] = mapped_column(nullable=True)
+
+
+if  __name__ == "__main__":
+    # Base.metadata.create_all(engine)
+    author1 = AuthorSQL(fullname="John Doe",
+                        birth_date="2000-01-01",
+                        birth_location="USA",
+                        description="A mysterious")
+    author1.save()
+    tag1 = TagSQL(tag="greeting")
+    tag2 = TagSQL(tag="world")
+    tag1.save()
+    tag2.save()
+    quote1 = QuoteSQL(author=author1,
+                      quote="Hello, World!",
+                      tags=["greeting", "world"])
+    quote1.save()
