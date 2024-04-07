9d04215920ca4719ef1c3d4875ccc4bb03d19cb9
dmytrot <ug.dotenko@gmail.com>
2024-04-07 10:51:46 +0300

    update

--- hw10/app_quotes/views.py
+++ hw10/app_quotes/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout as dj_logout
+from django.contrib.auth import login as dj_login
 
 from .models import Author, Quote, Tag
 
@@ -32,22 +36,57 @@ def tags(request,
 
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect("app_quotes:home")
     if request.method == 'POST':
-        return render(request=request,
-                      template_name='app_quotes/index.html',
-                      context={'msg': "Login handler"})
+        print('POST')
+        user = authenticate(username=request.POST['username'],
+                            password=request.POST['password'])
+        print(user)
+        if user is None:
+            print('user is None')
+            return render(request=request,
+                          template_name='app_quotes/login.html',
+                          context={'err': "Invalid credentials"})
+        dj_login(request, user)
+        return redirect("app_quotes:home")
+    print('GET login')
     return render(request=request,
-                  template_name='app_quotes/login.html',
-                  context={'msg': "Login page"})
+                    template_name='app_quotes/login.html',
+                    context={})
 
 
 def logout(request):
+    if request.user.is_authenticated:
+        dj_logout(request)
+        return redirect('app_quotes:home')
     return render(request=request,
                   template_name='app_quotes/index.html',
-                  context={'msg': "Logout handler"})
+                  context={})
 
 
 def register(request):
+    if request.user.is_authenticated:
+        return redirect("app_quotes:home")
+    if request.method == 'POST':
+        data_ = request.POST
+        if data_['password'] == data_['password2']:
+            try:
+                user = User.objects.create_user(username=data_['username'],
+                                                email=data_['email'],
+                                                password=data_['password'])
+
+            except Exception as e:
+                return render(request=request,
+                              template_name='app_quotes/register.html',
+                              context={'msg': f"User {data_['username']} already exists."})
+            user.save()
+            return redirect('app_quotes:login')
+
+        return render(request=request,
+                      template_name='app_quotes/register.html',
+                      context={'msg': "Passwords do not match"})
+
     return render(request=request,
                   template_name='app_quotes/register.html',
-                  context={'msg': "Signup page"})
+                   context={})
